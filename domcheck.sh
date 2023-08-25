ff10262b0045352b8830c2d3368e2f3aaa055484
arifsadiq <38719031+arifsadiq@users.noreply.github.com>
2023-08-25 10:10:43 +0400

    Update domcheck.sh

--- domcheck.sh
+++ domcheck.sh
@@ -9,8 +9,16 @@ ip=`dig +short $dom`
 ns=`dig +short $dom ns`
 mx=`dig +short $dom mx`
 
+# To check if the domain is vaild
+
+if [ -z "$ip" ]; then
+
+echo "The domain is not vaild"
+
+else
 echo "The IP address of $dom is $ip"
 echo
 echo "The nameserver of $dom are $ns"
 echo
 echo "The mail exchanger of $dom is $mx"
+fi
